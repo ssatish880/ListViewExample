776458f67bc4f482bba4bc0dadc1af27c3f58a36
Satish Sonwale <ssatish880@gmail.com>
2018-01-03 21:23:23 +0530

    Initial commit

---
+++ app/src/main/java/com/developers/shreeganesha/listviewexample/MainActivity.java
@@ -0,0 +1,37 @@
+package com.developers.shreeganesha.listviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        String[] titles = {"Android","Apple","SG Developers","Google"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,titles);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, "Clicked "+i, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
